c6eb0e40c73f55614c4e8e9a1ea2a0a125f79d90
Kauko <kauko@biosynth.link>
2025-01-11 01:48:39 -0800

    bksy != bsky

--- KaukoBskyFeeds.Shared/Bsky/BskyConsts.cs
+++ KaukoBskyFeeds.Shared/Bsky/BskyConsts.cs
@@ -4,7 +4,7 @@ public static class BskyConstants
 {
     public const string COLLECTION_TYPE_POST = "app.bsky.feed.post";
     public const string COLLECTION_TYPE_LIKE = "app.bsky.feed.like";
-    public const string COLLECTION_TYPE_REPOST = "app.bksy.feed.repost";
+    public const string COLLECTION_TYPE_REPOST = "app.bsky.feed.repost";
 
     public const string POST_EMBED_TYPE_IMAGES = "app.bsky.embed.images";
     public const string POST_EMBED_TYPE_VIDEO = "app.bsky.embed.video";
