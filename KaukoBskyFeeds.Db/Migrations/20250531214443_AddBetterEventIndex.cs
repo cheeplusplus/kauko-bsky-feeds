1e23651a80f4faa695fd3220d06fd90648de86f6
Kauko <kauko@biosynth.link>
2025-05-31 14:45:46 -0700

    Add [Did, EventTime DESC] index to Posts
    Was doing [EventTime DESC, Did] before which was incorrect

---
+++ KaukoBskyFeeds.Db/Migrations/20250531214443_AddBetterEventIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KaukoBskyFeeds.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBetterEventIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Did_EventTime",
+                table: "Posts",
+                columns: new[] { "Did", "EventTime" },
+                descending: new[] { false, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Did_EventTime",
+                table: "Posts");
+        }
+    }
+}
